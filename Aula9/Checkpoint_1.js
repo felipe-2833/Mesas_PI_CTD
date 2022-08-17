3f2b1f4c2204ef5b8670726908bd333c2ad4468d
felipe-2833 <69013950+felipe-2833@users.noreply.github.com>
2022-08-17 13:21:30 -0300

    Checkpoint 1

---
+++ Aula9/Checkpoint_1.js
@@ -0,0 +1,50 @@
+//criar função microondas;
+//criar dois parametros: comida e tempoPadão;
+//estruturar 5 comidas, com 5 tempos padões;
+//criar if's para as condições;
+//colocar console.log  "Prato pronto, bom apetite!!!". no final;
+
+function microondas (comida,tempo){
+    let tempoPadão;
+    switch (comida){
+        case 'Pipoca':
+            tempoPadão = 10;
+            break;
+        
+        case 'Macarrão':
+            tempoPadão = 8;
+            break;
+        
+        case 'Carne':
+            tempoPadão = 15;
+            break;
+
+        case 'Feijão':
+             tempoPadão = 12;
+             break;
+
+        case 'Brigadeiro':
+            tempoPadão = 12;
+            break;
+
+        default:
+            console.log("Prato inexistente");
+    }
+
+    if (tempo >= tempoPadão * 2 && tempo < tempoPadão * 3){
+        console.log('A comida queimou.');
+    }
+
+    else if (tempo < tempoPadão){
+        console.log("Tempo insuficiente.");
+    }
+
+    else if (tempo >= tempoPadão * 3){
+        console.log('KABOOOM!!!');
+    }
+
+    return microondas
+}
+
+microondas('Carne',45);
+console.log( "Prato pronto, bom apetite!!!");
