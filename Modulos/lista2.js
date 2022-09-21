767a707d61a68afede768cfeb1eeec1f59c28a92
felipe-2833 <69013950+felipe-2833@users.noreply.github.com>
2022-09-20 22:05:28 -0300

    arrumando

--- Modulos/lista2.js
+++ Modulos/lista2.js
@@ -3,6 +3,7 @@ let Lista2 = {
     //1)A média das idades das pessoas que responderam ótimo;
 
     mediaIdadeOtimo: function(array){
+
     let otimo = array.filter(function(opiniao){return opiniao.opiniao === 3});
     let media = 0;
     for(let i = 0; i < otimo.length; i++){
@@ -10,22 +11,25 @@ let Lista2 = {
     }
     return media/otimo.length
     
-},
+    },
 
-//2)A quantidade de pessoas que responderam regular;
+    //2)A quantidade de pessoas que responderam regular;
 
-qtdRegular: function (array){
+    qtdRegular: function (array){
+
     let regular = array.filter(function(opiniao){return opiniao.opiniao === 1});
     return regular.length
-},
 
-//3)A porcentagem de pessoas que responderam bom entre todos os espectadores analisados.
+    },
 
-porcentagemBom: function(array){
+    //3)A porcentagem de pessoas que responderam bom entre todos os espectadores analisados.
+
+    porcentagemBom: function(array){
+
     let bom = array.filter(function(opiniao){return opiniao.opiniao === 2});
     return (bom.length * 100)/array.length + '%'
     
-}
+    }
 };
 
 module.exports = Lista2;
